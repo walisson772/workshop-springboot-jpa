674b768420cc2b097035194b0ffd8c9a1bbb6ea2
Walisson Santos <walissonsantost008@gmail.com>
2025-05-11 22:06:09 -0300

    Craindo a class UserResource

---
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.educandoweb.course.resources;
+
+import com.educandoweb.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> finAll(){
+        User u = new User(1l, "walisson", "EEEEE.gmail.com", "9999999", "123");
+        return ResponseEntity.ok().body(u);
+    }
+}
